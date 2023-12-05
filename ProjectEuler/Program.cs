d26fecc7e8c4319adb089f91a9701c520db4a410
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2023-12-04 21:49:26 -0600

    Buscamos el valor solicitado.

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -8,6 +8,7 @@ namespace PE {
             Console.WriteLine($"Problema 001: {level01.Problem001(1_000)}");
             Console.WriteLine($"Problema 002: {level01.Problem002(4_000_000)}");
             Console.WriteLine($"Problema 003: {level01.Problem003(600_851_475_143)}");
+            Console.WriteLine($"Problema 004: {level01.Problem004(3)}");
         }
     }
 }
