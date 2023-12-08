c81b0325f66956575715e6111055603b2c6494f5
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2023-12-08 15:35:09 -0600

    Prettify

--- ProjectEuler.Test/Leve01_Tests.cs
+++ ProjectEuler.Test/Leve01_Tests.cs
@@ -60,12 +60,10 @@ namespace Project.Euler.Test {
 
         [Theory]
         [InlineData(4, 5832)]
-        [InlineData(13, 23514624000)]
+        [InlineData(13, 23_514_624_000)]
         public void Problem008_CalibrationTest(int digit, decimal want) {
             decimal got = problem.Problem008(digit);
             Assert.Equal(want, got);
         } 
-
-    
     }
 }
